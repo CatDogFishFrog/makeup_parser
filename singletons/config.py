8095e51c0744fd9012662da2090b3e7914251bb1
Vadim Goncharov <vadim.goncharov.1999@gmail.com>
2024-11-05 15:42:21 +0200

    Release 1.0.1

--- singletons/config.py
+++ singletons/config.py
@@ -2,7 +2,7 @@ import os
 import json
 
 
-class Config():
+class Config:
     CONFIG_PATH = 'config.json'
 
     _instance = None
@@ -24,9 +24,7 @@ class Config():
                 "output_file": "out_table.xlsx",
                 "currency_url": "https://obmennovosti.info/city.php?city=45",
                 "log_file": "app.log",
-                "error_file": "errors.txt",
-                "parser_delay": 3,
-                "log_level": "INFO",
+                "log_level": "WARNING",
                 "user_agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991",
                 "usd_regex": r'"USD","quoted":"UAH","bid":"([\d.]+)","ask"'
             }
@@ -36,18 +34,3 @@ class Config():
 
     def get(self, key: str):
         return self._config_data.get(key)
-
-# Приклад використання
-if __name__ == "__main__":
-    config = Config()
-    print(config.get("input_file"))
-    print(config.get("output_file"))
-    print(config.get("currency_url"))
-    print(config.get("log_file"))
-    config2 = Config()
-    config3 = Config()
-    print(config.get("error_file"))
-    print(config.get("parser_delay"))
-    print(config.get("log_level"))
-    print(config.get("user_agent"))
-    print(config.get("usd_regex"))
